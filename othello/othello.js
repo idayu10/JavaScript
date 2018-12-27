7f749ab421b0db75706c471935b5c92272613196
idayu10 <idayu1021@gmail.com>
2018-12-27 22:14:39 +0900

    js:othello 基本的な部分は完成

--- othello/othello.js
+++ othello/othello.js
@@ -4,25 +4,26 @@ var EMPTY = 0;
 var BLACK = 1;
 var WHITE = 2;
 var WALL = 5;
-var board = [];
+var boardG = [];
 var player;
 var enemy;
 
 function init(){
     for(var i = 0; i < BOARD_Y+2; i++){
-        board[i] = [];
+        boardG[i] = [];
     }
     drawBoard(BOARD_X, BOARD_Y);
-    board[4][4] = BLACK;
-    board[4][5] = WHITE;
-    board[5][4] = WHITE;
-    board[5][5] = BLACK;
+    boardG[4][4] = WHITE;
+    boardG[4][5] = BLACK;
+    boardG[5][4] = BLACK;
+    boardG[5][5] = WHITE;
     drawStones();
-    player = 1;
-    enemy = 2;
+    player = BLACK;
+    enemy = WHITE;
     var playerName = getById("current-player-name");
     playerName.innerHTML = "黒";
-    showArea();    
+    showArea();
+    countStones();
 }
 
 function getById(id){
@@ -55,7 +56,7 @@ function drawBoard(x, y){
             
             if(i == 0 || i == x + 1 || j == 0 || j == y + 1){
                 //cellに5を追加
-                board[i][j] = WALL;
+                boardG[i][j] = WALL;
             }else{
                 //cellの追加
                 var cell = row.insertCell(-1);
@@ -63,7 +64,7 @@ function drawBoard(x, y){
                 cell.setAttribute("id", getCellId(j, i))
                 cell.setAttribute("onclick", "clicked(this)")
                 //cellに0を追加
-                board[j][i] = EMPTY;
+                boardG[i][j] = EMPTY;
             }
         }
     }
@@ -73,31 +74,36 @@ function drawBoard(x, y){
 }
 
 function drawStones(){
-    for(var i = 1; i < BOARD_X+1; i++){
-        for(var j = 1; j < BOARD_Y+1; j++){
-            var cell = getById(getCellId(i, j));
+    for(var i = 1; i < BOARD_Y+1; i++){
+        for(var j = 1; j < BOARD_X+1; j++){
+            var cell = getById(getCellId(j, i));
             cell.innerHTML = "";
-                if(board[i][j] == BLACK){
-                    cell.innerHTML = "●";
-                    cell.style.color = "black";
-                }
-                if(board[i][j] == WHITE){
-                    cell.innerHTML = "●";
-                    cell.style.color = "white";
-                }
+            if(boardG[i][j] == BLACK){
+                cell.innerHTML = "●";
+                cell.style.color = "black";
+            }
+            if(boardG[i][j] == WHITE){
+                cell.innerHTML = "●";
+                cell.style.color = "white";
+            }
         }
     }
 }
 
-function canPut(x,y,isPut){
+function canPut(x,y,isPut,board){
+
+    if(!board){
+        board = boardG;
+    }
+
     var canPut = false;
-    if(board[x][y] == 0){
+    if(board[y][x] == 0){
         for (var dx = -1; dx <= 1; dx++) {
             for (var dy = -1; dy <= 1; dy++) {
-                if(enemy == board[x+dx][y+dy]){
-                    if(canSandwitch(x,y,dx,dy)){
+                if(enemy == board[y+dy][x+dx]){
+                    if(canSandwitch(x,y,dx,dy,board)){
                         if(isPut){
-                            turnStones(x,y,dx,dy);
+                            turnStones(x,y,dx,dy,board);
                         }
                         canPut = true;
                     }
@@ -108,15 +114,15 @@ function canPut(x,y,isPut){
     return canPut;
 }
 
-function canSandwitch(x,y,dx,dy){
+function canSandwitch(x,y,dx,dy,board){
     isPlayer = false;
     var i = 1;
     while(true){
-        if(board[x+dx*i][y+dy*i] == player){
+        if(board[y+dy*i][x+dx*i] == player){
             isPlayer = true;
             break;
         }
-        if(board[x+dx*i][y+dy*i] == WALL || board[x+dx*i][y+dy*i] == EMPTY){
+        if(board[y+dy*i][x+dx*i] == WALL || board[y+dy*i][x+dx*i] == EMPTY){
             break;
         }
         i++;
@@ -129,12 +135,25 @@ function clicked(element){
     var id = element.getAttribute("id");
     var x = getX(id);
     var y = getY(id);
-    if(canPut(Number(x),Number(y), true)){
+    if(canPut(Number(x),Number(y), true,null)){
         switchColor();
         drawStones();
         showArea();
+        countStones();
     }else{
         alert("置けない");
+        return;
+    }
+
+    if(isPass()){
+        switchColor();
+        showArea();
+        countStones();
+        return;
+    }
+
+    if(isFinish()){
+        return;
     }
 
     var blackPlayer = document.forms.settigs.black.value;
@@ -144,22 +163,61 @@ function clicked(element){
         return;
     }
 
-    if(blackPlayer == "human"){
-        switch (whitePlayer) {
-            case "cpu1":
-                putCpu1();
-                break;
-        
-        }
-    }
+    // isPass=trueの場合ループ
+    while(true){
 
-    if(whitePlayer == "human"){
-        switch (blackPlayer) {
-            case "cpu1":
-                putCpu1();
-                break;
-        
+        if(blackPlayer == "human" && player == WHITE){
+            switch (whitePlayer) {
+                case "cpu1":
+                    putCpu1();
+                    break;
+                case "cpu2":
+                    putCpu2();
+                    break;
+                case "cpu3":
+                    putCpu3();
+                    break;
+                case "cpu4":
+                    putCpu4();
+                    break;
+                case "cpu5":
+                    putCpu5();
+                    break;
+            
+            }
         }
+    
+        if(whitePlayer == "human" && player == BLACK){
+            switch (blackPlayer) {
+                case "cpu1":
+                    putCpu1();
+                    break;
+                case "cpu2":
+                    putCpu2();
+                    break;
+                case "cpu3":
+                    putCpu3();
+                    break;
+                case "cpu4":
+                    putCpu4();
+                    break;
+                case "cpu5":
+                    putCpu5();
+                    break;
+            
+            }
+        }
+
+        if(isFinish()){
+            return;
+        }
+
+        if(!isPass()){
+            return;
+        }
+
+        alert("パス");
+        switchColor();
     }
 }
 
@@ -175,49 +233,96 @@ function switchColor(){
     }
 }
 
-function turnStones(x,y,dx,dy){
-    board[x][y] = player;
+function turnStones(x,y,dx,dy,board){
+    board[y][x] = player;
 
     var i = 1;
 
     while(true){
-        if(board[x+dx*i][y+dy*i] == player){
+        if(board[y+dy*i][x+dx*i] == player){
             break;
         }
-        board[x+dx*i][y+dy*i] = player;
+        board[y+dy*i][x+dx*i] = player;
 
         i++;
     }
 }
 
 function showArea(){
-    for(var i = 1; i < BOARD_X+1; i++){
-        for(var j = 1; j < BOARD_Y+1; j++){
-            var cell = getById(getCellId(i, j));
+    for(var i = 1; i < BOARD_Y+1; i++){
+        for(var j = 1; j < BOARD_X+1; j++){
+            var cell = getById(getCellId(j, i));
             cell.style.backgroundColor = "#090";
-            if(canPut(i,j,false)){
+            if(canPut(j,i,false,null)){
                 cell.style.backgroundColor = "red";
             }
         }
     }
 }
 
-function isFinish(){
-    var count = 0;
+function countStones(){
+    var blackStone = 0;
+    var whiteStone = 0;
 
-    for(var i = 1; i < BOARD_X+1; i++){
-        for(var j = 1; j < BOARD_Y+1; j++){
-            if(board[i][j] != 0){
-                count++;
+    for(var i = 1; i < BOARD_Y+1; i++){
+        for(var j = 1; j < BOARD_X+1; j++){
+            if(boardG[i][j] == BLACK){
+                blackStone++;
+            }
+            if(boardG[i][j] == WHITE){
+                whiteStone++;
             }
         }
     }
 
-    if(count == 64){
-        return true;
-    }else{
-        return false;
+    getById("blackStone").innerHTML = blackStone;
+    getById("whiteStone").innerHTML = whiteStone;
+
+    return blackStone + whiteStone;
+}
+
+function isPass(){
+
+    var isPass = true;
+
+    for(var i = 1; i < BOARD_Y+1; i++){
+        for(var j = 1; j < BOARD_X+1; j++){
+            if(canPut(j,i,false,null)){
+                isPass = false;
+            }
+        }
     }
+
+    return isPass;
+}
+
+function isFinish(){
+    var count = 0;
+    var me = false;
+    var enemy = false;
+
+    for(var i = 1; i < BOARD_Y+1; i++){
+        for(var j = 1; j < BOARD_X+1; j++){
+            if(boardG[i][j] != 0){
+                count++;
+            }
+
+            if(canPut(i,j,false,null)){
+                me = true;
+            }
+            switchColor();
+            if(canPut(i,j,false,null)){
+                enemy = true;
+            }
+            switchColor();
+        }
+    }
+
+    if(!me && !enemy){
+        return true;
+    }
+
+    return count == BOARD_X*BOARD_Y;
 }
 
 function btnPushed(element){
@@ -234,6 +339,18 @@ function btnPushed(element){
         case "cpu1":
             putCpu1();
             break;
+        case "cpu2":
+            putCpu2();
+            break;
+        case "cpu3":
+            putCpu3();
+            break;
+        case "cpu4":
+            putCpu4();
+            break;
+        case "cpu5":
+            putCpu5();
+            break;
     
     }
 }
@@ -241,37 +358,72 @@ function btnPushed(element){
 function initFromButton(blackPlayer,whitePlayer){
     //board初期化
     for(var i = 0; i < BOARD_Y+2; i++){
-        board[i] = [];
+        boardG[i] = [];
     }
     for(var i = 0; i < BOARD_Y+2; i++){
         for(var j = 0; j < BOARD_X+2; j++){
             if(i == 0 || i == BOARD_X + 1 || j == 0 || j == BOARD_Y + 1){
                 //cellに5を追加
-                board[i][j] = WALL;
+                boardG[j][i] = WALL;
             }else{
                 //cellに0を追加
-                board[j][i] = EMPTY;
+                boardG[j][i] = EMPTY;
             }
         }
     }
-    board[4][4] = BLACK;
-    board[4][5] = WHITE;
-    board[5][4] = WHITE;
-    board[5][5] = BLACK;
+    boardG[4][4] = WHITE;
+    boardG[4][5] = BLACK;
+    boardG[5][4] = BLACK;
+    boardG[5][5] = WHITE;
     drawStones();
-    player = 1;
-    enemy = 2;
+    player = BLACK;
+    enemy = WHITE;
     var playerName = getById("current-player-name");
     playerName.innerHTML = "黒";
     showArea(); 
+    countStones();
 }
 
 function doCpuVsCpu(){
 
+    var blackPlayer = document.forms.settigs.black.value;
+    var whitePlayer = document.forms.settigs.white.value;
+    var cpuColor;
+
     while(true){
-        putCpu1();
+
+        if(player == BLACK){
+            cpuColor = blackPlayer;
+        }else{
+            cpuColor = whitePlayer;
+        }
+
+        switch (cpuColor) {
+            case "cpu1":
+                putCpu1();
+                break;
+            case "cpu2":
+                putCpu2();
+                break;
+            case "cpu3":
+                putCpu3();
+                break;
+            case "cpu4":
+                putCpu4();
+                break;
+            case "cpu5":
+                putCpu5();
+                break;
+        
+        }
+
         if(isFinish()){
             break;
+        }
+
+        if(isPass()){
+            switchColor();
+            continue;
         }
     }
 }
@@ -280,7 +432,7 @@ function putCpu1(){
     var coordinates =[];
     for(var i = 1; i < BOARD_X+1; i++){
         for(var j = 1; j < BOARD_Y+1; j++){
-            if(canPut(i,j,false)){
+            if(canPut(i,j,false,null)){
                 var coordinate = {x:i,y:j};
                 coordinates.push(coordinate);
             }
@@ -288,9 +440,58 @@ function putCpu1(){
     }
 
     var coordinate = coordinates[Math.floor(Math.random() * coordinates.length)];
-    canPut(coordinate.x,coordinate.y,true);
+    canPut(coordinate.x,coordinate.y,true,null);
 
     switchColor();
     drawStones();
     showArea();
+    countStones();
+}
+
+function putCpu2(){
+    var weight = [];
+    weight.push([-99,-99,-99,-99,-99,-99,-99,-99,-99,-99]);
+    weight.push([-99,30,-12,0,-1,-1,0,-12,30,-99]);
+    weight.push([-99,-12,-15,-3,-3,-3,-3,-15,-12,-99]);
+    weight.push([-99,0,-3,0,-1,-1,-0,-3,0,-99]);
+    weight.push([-99,-1,-3,-1,-1,-1,-1,-3,-1,-99]);
+    weight.push([-99,-1,-3,-1,-1,-1,-1,-3,-1,-99]);
+    weight.push([-99,0,-3,0,-1,-1,-0,-3,0,-99]);
+    weight.push([-99,-12,-15,-3,-3,-3,-3,-15,-12,-99]);
+    weight.push([-99,30,-12,0,-1,-1,0,-12,30,-99]);
+    weight.push([-99,-99,-99,-99,-99,-99,-99,-99,-99,-99]);
+
+    var coordinates =[];
+    for(var i = 1; i < BOARD_X+1; i++){
+        for(var j = 1; j < BOARD_Y+1; j++){
+            if(canPut(i,j,false,null)){
+                var coordinate = {x:i,y:j};
+                coordinates.push(coordinate);
+            }
+        }
+    }
+
+    var max = -999;
+
+    for(var tmp of coordinates){
+        if(max < weight[tmp.y][tmp.x]){
+            max = weight[tmp.y][tmp.x];
+        }
+    }
+
+    var coordinatesMax = [];
+
+    for(var tmp of coordinates){
+        if(max == weight[tmp.y][tmp.x]){
+            coordinatesMax.push(tmp);
+        }
+    }
+
+    var coordinate = coordinatesMax[Math.floor(Math.random() * coordinatesMax.length)];
+    canPut(coordinate.x,coordinate.y,true,null);
+
+    switchColor();
+    drawStones();
+    showArea();
+    countStones();
 }
